efebc30344f985868c93eb0635e17f687ffcea5e
Sam <wk.sams@gmail.com>
2020-02-20 16:39:13 -0300

    User must be notified if anything other than a 0 or 1 was entered

--- scripts/script.js
+++ scripts/script.js
@@ -18,24 +18,39 @@ function copy() {
 }
 
 // Submit
+var input = $('#binaryInput');
 $('#binaryInput').keypress(e => {
-    if (e.which == '13')
+    if (e.which == '13') {
+        input.removeClass('is-invalid');
         convert();
+    }
+    else if (e.which == '48' || e.which == '49') {
+        input.removeClass('is-invalid');
+    } else {
+        // User must be notified if anything other than a 0 or 1 was entered
+        input.addClass('is-invalid');
+        return false;
+    }
 });
 
-// Convert
+// Converter
 function convert() {
-    var output = $('#output');
-    var baseOutput = parseInt(output.attr('base'));
-
     var input = $('#binaryInput');
     var baseInput = parseInt(input.attr('base'));
+    // Arrays may not be used to contain the binary digits entered by the user
     var value = input.val();
+
+    var output = $('#output');
+    var baseOutput = parseInt(output.attr('base'));
 
     switch(baseOutput) {
         case 10:
             var result = 0;
             var powVal = 0;
+            /* Determining the decimal equivalent of a particular binary digit 
+               in the sequence must be calculated using a single mathematical function, 
+               for example the natural logarithm. It's up to you to figure out 
+               which function to use. */
             for(var i = value.length - 1; i >= 0; i--) {
                 result += parseInt(value[i]) * Math.pow(baseInput, powVal);
                 powVal++;
@@ -43,4 +58,7 @@ function convert() {
             break;
     }
     output.val(result);
+    input.val('');
 };
+
+// TODO: User must be notified if anything other than a 0 or 1 was entered
