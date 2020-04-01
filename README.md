15580c0bb013b73c1b43ef7cf458f39f11f090cf
Sam <wk.sams@gmail.com>
2020-04-01 18:16:09 -0300

    user can change color and intensity

--- README.md
+++ README.md
@@ -13,12 +13,12 @@ displayed during the Christmas Holidays.
 ## User Stories
 
 -   [x] User can press a button to start and stop the display
--   [ ] User can change the interval of time controlling the change in intensity
+-   [x] User can change the interval of time controlling the change in intensity
 
 ## Bonus features
 
--   [ ] User can select the color used to fill each circle
--   [ ] User can specify the intensity value
+-   [x] User can select the color used to fill each circle
+-   [x] User can specify the intensity value
 -   [ ] User can change the size of any circle in the row
 -   [ ] User can specify the number of rows to be included in the display. From
         one to seven rows can be chosen
--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,80 @@
-const header = $("h1");
-const bulbs  = [$(".bulb-1"), $(".bulb-2"), $(".bulb-3"), $(".bulb-4"), $(".bulb-5"), $(".bulb-6"), $(".bulb-7")];
-const colors = ["bulb1", "bulb2", "bulb3", "bulb4", "bulb5", "bulb6", "bulb7"];
+const root = document.documentElement;
 
 $(".power").click(function() {
     toggleLight($(this));
+    $(".collapse").collapse('hide');
 });
 
 function toggleLight(e) {
+    const header = $("h1");
     if(e.hasClass("text-on")) {
         e.addClass("text-off");
         header.addClass("text-off");
-        $.each(bulbs, function(index, bulb) {
-            bulb.addClass("off");
-        });
-
+        
         e.removeClass("text-on");
         header.removeClass("text-on");
-        $.each(bulbs, function(index, bulb) {
-            bulb.removeClass(colors[index]);
+        $(".bulb").each(function(index) {
+            $(this).removeClass("twinkle-" + (index + 1));
         });
+
+        $(".bulb").css("cursor", "default");
+        isOn = false;
     } else {
         e.addClass("text-on");
         header.addClass("text-on");
-        $.each(bulbs, function(index, bulb) {
-            bulb.addClass(colors[index]);
+        $(".bulb").each(function(index) {
+            $(this).addClass("twinkle-" + (index + 1));
         });
         
         e.removeClass("text-off");
         header.removeClass("text-off");
-        $.each(bulbs, function(index, bulb) {
-            bulb.removeClass("off");
-        });
+
+        $(".bulb").css("cursor", "pointer");
+        isOn = true;
+    }
+}
+
+let bulbId = 1;
+let isOn = true;
+$(".bulb").click(function() {
+    if(isOn) {
+        bulbId = $(this).data("bulb-id");
+        $(".collapse").collapse('show');
+
+        const spreadValue = getComputedStyle(document.documentElement).getPropertyValue("--spread-" + bulbId).replace("px", "").replace(" ", "");
+        output.innerHTML = spreadValue;
+        slider.value = spreadValue;
+    }
+});
+
+$(".color").click(function() {
+    if(isOn) {
+        root.style.setProperty("--twinkle-color-" + bulbId, $(this).css("background-color"));
+        root.style.setProperty("--twinkle-dark-color-" + bulbId, darken($(this).css("background-color"), 0.5));
+    }
+});
+
+function darken(color, amount=0.5) {
+    const rgb = color.replace(/^(rgb|rgba)\(/,'').replace(/\)$/,'').replace(/\s/g,'').split(',');
+    for (let i = 0; i < rgb.length; i++) {
+        rgb[i] = Math.floor(rgb[i] * amount);
+    }
+    return "rgb(" + rgb[0] + ", " + rgb[1] + ", " + rgb[2] + ")";
+}
+
+$(document).click(function(e) {
+    if(!$(e.target).closest(".card, .bulb").length) {
+        $(".collapse").collapse('hide');
+    }
+});
+
+var slider = document.getElementById("intensity");
+var output = document.getElementById("demo");
+
+slider.oninput = function() {
+    output.innerHTML = this.value;
+    if(isOn) {
+        root.style.setProperty("--spread-" + bulbId, this.value + "px");
+        root.style.setProperty("--blur-" + bulbId, (this.value * 2) + "px");
     }
 }
