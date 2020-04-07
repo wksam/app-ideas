1103c004779476c0885d1f29b4418e8f092c1d82
Sam <wk.sams@gmail.com>
2020-04-07 11:09:19 -0300

    countdown working with multiple countdowns

--- README.md
+++ README.md
@@ -25,7 +25,7 @@ own original implementation.
 
 -   [x] User can see an event input box containing an event name field, an
 date field, an optional time, and a 'Start' button.
--   [ ] User can define the event by entering its name, the date it is
+-   [x] User can define the event by entering its name, the date it is
 scheduled to take place, and an optional time of the event. If the time is 
 omitted it is assumed to be at Midnight on the event date in the local time
 zone.
@@ -34,9 +34,9 @@ zone.
 entered. 
 -   [ ] User can see a warning message if the time until the event data and time
 that has been entered would overflow the precision of the countdown timer.
--   [ ] User can click on the 'Start' button to see the countdown timer start
+-   [x] User can click on the 'Start' button to see the countdown timer start
 displaying the days, hours, minutes, and seconds until the event takes place.
--   [ ] User can see the elements in the countdown timer automatically
+-   [x] User can see the elements in the countdown timer automatically
 decrement. For example, when the remaining seconds count reaches 0 the remaining
 minutes count will decrement by 1 and the seconds will start to countdown from 59. This progression must take place from seconds all the way up to the remaining days position in countdown display. 
 
@@ -44,8 +44,8 @@ minutes count will decrement by 1 and the seconds will start to countdown from 5
 
 -   [ ] User can save the event so that it persists across sessions
 -   [ ] User can see an alert when the event is reached
--   [ ] User can specify more than one event. 
--   [ ] User can see a countdown timers for each event that has been defined.
+-   [x] User can specify more than one event. 
+-   [x] User can see a countdown timers for each event that has been defined.
 
 ## Useful links and resources
 
---
+++ script.js
@@ -0,0 +1,117 @@
+const SECOND = 1000;
+const MINUTE = SECOND * 60;
+const HOUR   = MINUTE * 60;
+const DAY    = HOUR * 24;
+
+function createCountdown(name) {
+    const html = 
+        '<div class="countdown d-flex flex-row">' +
+            '<div class="d-flex flex-column p-2">' +
+                '<div class="text-center">' + name + '</div>' +
+            '</div>' +
+            '<div class="d-flex flex-column p-2">' +
+                '<div class="text-center days">0</div>' +
+                '<div class="text-center">days</div>' +
+            '</div>' +
+            '<div class="d-flex flex-column p-2">' +
+                '<div class="text-center hours">0</div>' +
+                '<div class="text-center">hours</div>' +
+            '</div>' +
+            '<div class="d-flex flex-column p-2">' +
+                '<div class="text-center">:</div>' +
+            '</div>' +
+            '<div class="d-flex flex-column p-2">' +
+                '<div class="text-center minutes">0</div>' +
+                '<div class="text-center">minutes</div>' +
+            '</div>' +
+            '<div class="d-flex flex-column p-2">' +
+                '<div class="text-center">:</div>' +
+            '</div>' +
+            '<div class="d-flex flex-column p-2">' + 
+                '<div class="text-center seconds">0</div>' +
+                '<div class="text-center">seconds</div>' +
+            '</div>' +
+            '<div class="d-flex flex-column p-2">' +
+                '<button class="btn btn-primary">' +
+                    '<svg class="bi bi-trash-fill" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">' +
+                        '<path fill-rule="evenodd" d="M2.5 1a1 1 0 00-1 1v1a1 1 0 001 1H3v9a2 2 0 002 2h6a2 2 0 002-2V4h.5a1 1 0 001-1V2a1 1 0 00-1-1H10a1 1 0 00-1-1H7a1 1 0 00-1 1H2.5zm3 4a.5.5 0 01.5.5v7a.5.5 0 01-1 0v-7a.5.5 0 01.5-.5zM8 5a.5.5 0 01.5.5v7a.5.5 0 01-1 0v-7A.5.5 0 018 5zm3 .5a.5.5 0 00-1 0v7a.5.5 0 001 0v-7z" clip-rule="evenodd"/>' +
+                    '</svg>' +
+                '</button>' +
+            '</div>' +
+        '</div>';
+    $(".container").append(html);
+    const lastElement = $(".countdown").last();
+    lastElement.find("button").on("click", remove);
+    return lastElement.uniqueId().attr("id");
+}
+
+$("form").submit(function(e) {
+    const submit = $(this).serializeArray();
+    const eventName = getData(submit, "event");
+
+    let inputDate = getData(submit, "date");
+    let inputTime = getData(submit, "time");
+    inputTime = inputTime != "" ? inputTime : "00:00";
+    const date = new Date(formatDate(inputDate, inputTime)).getTime();
+
+    const id = createCountdown(eventName);
+    const intervalId = setInterval(function() { updateTimer(id, date) }, 1000);
+    $("#" + id).data("intervalId", intervalId);
+
+    e.preventDefault();
+});
+
+function remove() {
+    const id = getCountdownId($(this));
+    const intervalId = getCountdownIntervalId($(this));
+    const countdown = $("#" + id);
+
+    clearInterval(intervalId);
+    countdown.removeUniqueId();
+    countdown.remove();
+}
+
+function getCountdownId(e) {
+    return e.closest(".countdown").attr("id");
+}
+
+function getCountdownIntervalId(e) {
+    return e.closest(".countdown").data("intervalId");
+}
+
+function getData(array, name) {
+    for(let i = 0; i < array.length; i++) {
+        if(array[i].name == name) return array[i].value;
+    }
+}
+
+// format ECMAScript 5 ISO-8601
+function formatDate(date, time) {
+    return date + "T" + time;
+}
+
+function updateTimer(id, date) {
+    const now = new Date().getTime();
+    
+    const remaining = date - now;
+    let days = parseInt(remaining / DAY);
+    let hours = parseInt(remaining % DAY / HOUR);
+    let minutes = parseInt(remaining % HOUR / MINUTE);
+    let seconds = parseInt(remaining % MINUTE / SECOND);
+    days = days < 0 ? 0 : days;
+    hours = hours < 0 ? "00" : addZero(hours);
+    minutes = minutes < 0 ? "00" : addZero(minutes);
+    seconds = seconds < 0 ? "00" : addZero(seconds);
+
+    $("#" + id).find(".days").html(days);
+    $("#" + id).find(".hours").html(hours);
+    $("#" + id).find(".minutes").html(minutes);
+    $("#" + id).find(".seconds").html(seconds);
+}
+
+function addZero(number) {
+    if(number < 10) {
+        return "0" + number;
+    }
+    return number;
+}
