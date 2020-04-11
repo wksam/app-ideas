8a4df4d7daa1e460bf80dffef0666fff7fa42b56
Sam <wk.sams@gmail.com>
2020-04-11 13:17:53 -0300

    any image can be flipped vertically and horizontally

--- script.js
+++ script.js
@@ -12,3 +12,35 @@ function initPane() {
 }
 
 initPane();
+
+const bodyStyle = window.getComputedStyle(document.body);
+
+$('button').click(function(e) {
+    const card = $(this).parents('.card');
+    changeRotation($(this), card);
+    changeScale($(this), card);
+});
+
+function changeRotation(btn, card) {
+    if(btn.hasClass('horizontal'))
+        card.toggleClass('rotate-x');
+    else
+        card.toggleClass('rotate-y');
+}
+
+function changeScale(btn, card) {
+    if(btn.hasClass('horizontal'))
+        if(card.hasClass('scale'))
+            card.removeClass('scale').addClass('scale-x');
+        else if(card.hasClass('scale-x'))
+            card.removeClass('scale-x').toggleClass('scale');
+        else
+            card.toggleClass('scale-y');
+    else
+        if(card.hasClass('scale'))
+            card.removeClass('scale').addClass('scale-y');
+        else if(card.hasClass('scale-y'))
+            card.removeClass('scale-y').toggleClass('scale');
+        else
+            card.toggleClass('scale-x');
+}
