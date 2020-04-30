3b245f88fea0e6627772717fbbe8b2892b37d3e5
Sam <wk.sams@gmail.com>
2020-04-30 09:41:56 -0300

    remove log and add roman to decimal converter project

--- roman-to-decimal-converter/scripts/script.js
+++ roman-to-decimal-converter/scripts/script.js
@@ -82,7 +82,6 @@ function convertToDecimal(romanNumber) {
     return result;
 }
 
-console.log(convertToRoman(2421));
 function convertToRoman(decimalNumber) {
     const decimalTable = [{ 1000: 'M' }, { 900: 'CM' }, { 500: 'D' }, { 400: 'CD' }, { 100: 'C' }, { 90: 'XC' }, { 50: 'L' }, { 40: 'XL' }, { 10: 'X' }, { 9: 'IX' }, { 5: 'V' }, { 4: 'IV' }, { 1: 'I' }];
     let result = '';
