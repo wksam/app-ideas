8dd36b4101a8e0ecbaf9c4d7fb359788b87134f8
Sam <wk.sams@gmail.com>
2020-05-17 10:00:50 -0300

    calculate wind chill

--- windchill/README.md
+++ windchill/README.md
@@ -10,7 +10,7 @@ temperature.
 
 -   [x] User can select the measurement system calculations will be performed in - Metric or English
 -   [x] User can enter the actual temperature and the wind speed
--   [ ] User can press the `Calculate` button to display the wind chill
+-   [x] User can press the `Calculate` button to display the wind chill
 -   [x] User will receive an error message when `Calculate` is clicked if data values are not entered
 
 ## Bonus features
--- windchill/script.js
+++ windchill/script.js
@@ -7,9 +7,36 @@ function onChangeTypeSystem() {
     const windSpeedAddon = document.querySelector('#speed-addon');
     if(this.value != 'imperial') {
         temperatureAddon.textContent = '°C';
-        windSpeedAddon.textContent = 'm/s';
+        windSpeedAddon.textContent = 'km/h';
     } else {
         temperatureAddon.textContent = '°F';
         windSpeedAddon.textContent = 'mph';
     }
+}
+
+document.querySelector('form').addEventListener('submit', startCalculation);
+
+function startCalculation(e) {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const system = formData.get('system');
+    const temperature = formData.get('temperature');
+    const windSpeed = formData.get('speed');
+    const result = system == 'metric' ? Math.round(metricCalculation(temperature, windSpeed)) : Math.round(imperialCalculation(temperature, windSpeed));
+    
+    let resultText = 'The wind chill index is ' + result + ', based on the TS scale.';
+    resultText = system == 'metric' ? resultText.replace('TS', 'Celsius') : resultText.replace('TS', 'Fahrenheit')
+
+    const alert = document.querySelector('.alert');
+    alert.textContent = resultText;
+    alert.hidden = false;
+}
+
+function metricCalculation(temperature, windSpeed) {
+    return 13.12 + 0.6215 * temperature - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temperature * Math.pow(windSpeed, 0.16);
+}
+
+function imperialCalculation(temperature, windSpeed) {
+    return 35.74 + 0.6215 * temperature - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temperature * Math.pow(windSpeed, 0.16);
 }
