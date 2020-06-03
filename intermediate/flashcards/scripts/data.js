7d6ccbaf9c9ae840e73e3382e01600d476fc337b
Sam <wk.sams@gmail.com>
2020-06-03 14:33:48 -0300

    add layout

---
+++ intermediate/flashcards/scripts/data.js
@@ -0,0 +1,18 @@
+const deck = [{
+    question: 'What does the acronym CDN stand for in terms of networking?',
+    answer: 'Content Delivery Network',
+    choices: [
+        "Content Distribution Network",
+        "Computational Data Network",
+        "Compressed Data Network"
+    ],
+    question: 'In the programming language &quot;Python&quot;, which of these statements would display the string &quot;Hello World&quot; correctly?',
+    answer: 'print(&quot;Hello World&quot;)',
+    choices: [
+        "console.log(&quot;Hello World&quot;)",
+        "echo &quot;Hello World&quot;",
+        "printf(&quot;Hello World&quot;)"        
+    ]
+}];
+
+Object.freeze(deck);
