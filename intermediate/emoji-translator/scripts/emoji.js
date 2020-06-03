48065de2bb1c986755284af358416a3bf51d1a36
Sam <wk.sams@gmail.com>
2020-06-02 22:39:25 -0300

    add emoji table

---
+++ intermediate/emoji-translator/scripts/emoji.js
@@ -0,0 +1,47 @@
+(function() {
+    const table = document.querySelector('tbody');
+    let count = 1;
+
+    Object.entries(emojis).forEach(([name, vendors]) => {
+        const row = document.createElement('tr');
+        const nEmoji = document.createElement('th');
+        nEmoji.setAttribute('scope', 'row');
+        nEmoji.textContent = count;
+        row.append(nEmoji);
+
+        const textData = document.createElement('td');
+        textData.textContent = name;
+        row.append(textData);
+
+        if(Object.keys(vendors).length < 7) {
+            const emojiData = document.createElement('td');
+            emojiData.setAttribute('colspan', 11);
+            Object.entries(vendors).forEach(([vendor, emoji]) => {
+                if(vendor === 'browser') {
+                    const browserData = document.createElement('td');
+                    browserData.textContent = emoji;
+                    row.append(browserData);
+                } else {
+                    const emojiImg = document.createElement('img');
+                    emojiImg.setAttribute('src', emoji);
+                    emojiData.append(emojiImg);
+                }
+            });
+            row.append(emojiData);
+        } else {
+            Object.entries(vendors).forEach(([vendor, emoji]) => {
+                const emojiData = document.createElement('td');
+                if(emoji.search('data') !== -1) {
+                    const emojiImg = document.createElement('img');
+                    emojiImg.setAttribute('src', emoji);
+                    emojiData.append(emojiImg);
+                } else {
+                    emojiData.textContent = emoji;
+                }
+                row.append(emojiData);
+            });
+        }
+        table.append(row);
+        count++;
+    });
+})();
