e0c9f803d55e808123b2090105694a6553fe3a65
Sam <wk.sams@gmail.com>
2020-06-15 18:52:19 -0300

    add markdown paragraphs

--- intermediate/markdown-previewer/script.js
+++ intermediate/markdown-previewer/script.js
@@ -6,16 +6,30 @@ function onChangeText(e) {
 }
 
 function markdown(text) {
+    // Heading
     for (let i = 1; i <= 6; i++) {
         const regex = new RegExp('^(#{' + i + '} .*)$', 'gm');
-        if(text.match(regex) != null) text = headingsReplace(text, text.match(regex).map(match => match.replace(/[\r\n|\r|\n]/)), i);
+        if(text.match(regex) != null)
+            text = headings(text, text.match(regex).map(match => match.replace(/[\r\n|\r|\n]/)), i);
+    }
+
+    // Paragraphs
+    if(text.match(/^[^#{1,6}|\n]+$/gm))
+        text = paragraphs(text, text.match(/^[^#{1,6}|\n]+$/gm));
+    return text;
+}
+
+function headings(text, matches, level) {
+    for (const match of matches) {
+        text = text.replace(match, '<h' + level + '>' + match.substring(level + 1, text.length) + '</h' + level + '>');
     }
     return text;
 }
 
-function headingsReplace(text, matches, level) {
+function paragraphs(text, matches) {
     for (const match of matches) {
-        text = text.replace(match, '<h' + level + '>' + match.substring(level, text.length) + '</h' + level + '>');
+        const regex = new RegExp('^' + match + '$', 'gm');
+        text = text.replace(regex, '<p>' + match + '</p>');
     }
     return text;
 }
