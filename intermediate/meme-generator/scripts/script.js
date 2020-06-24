c1c5e4ee122a3ea3bb3bfe47eb341b92d4c56162
Sam <wk.sams@gmail.com>
2020-06-24 14:48:40 -0300

    user can select the color of the text

--- intermediate/meme-generator/scripts/script.js
+++ intermediate/meme-generator/scripts/script.js
@@ -2,18 +2,25 @@
     document.querySelector('#image-url').addEventListener('input', onChangeUrl);
     function onChangeUrl() {
         imageUrl = this.value;
-        redraw();
     }
 
     document.querySelector('#top-text').addEventListener('input', onChangeTopText);
     function onChangeTopText() {
         topText = this.value;
-        redraw();
+    }
+
+    document.querySelector('#top-text-color').addEventListener('input', onChangeTopTextColor);
+    function onChangeTopTextColor() {
+        topTextColor = this.value;
     }
 
     document.querySelector('#bottom-text').addEventListener('input', onChangeBottomText);
     function onChangeBottomText() {
         bottomText = this.value;
-        redraw();
+    }
+
+    document.querySelector('#bottom-text-color').addEventListener('input', onChangeBottomTextColor);
+    function onChangeBottomTextColor() {
+        bottomTextColor = this.value;
     }
 })();
--- intermediate/meme-generator/scripts/sketch.js
+++ intermediate/meme-generator/scripts/sketch.js
@@ -1,6 +1,10 @@
 let imageUrl;
-let topText;
-let bottomText;
+
+let topText = '';
+let topTextColor = '#000';
+
+let bottomText = '';
+let bottomTextColor = '#000';
 
 let width = 400;
 let height = 400;
@@ -10,7 +14,6 @@ function setup() {
     canvas.parent('image-editor');
 
     textSize(32);
-    noLoop();
 }
 
 function draw() {
@@ -29,8 +32,10 @@ function draw() {
     }
 
     textAlign(CENTER, TOP);
+    fill(topTextColor);
     text(topText, 0, 0, width, height / 2);
 
     textAlign(CENTER, BOTTOM);
+    fill(bottomTextColor);
     text(bottomText, 0, height / 2, width, height / 2);
 }
