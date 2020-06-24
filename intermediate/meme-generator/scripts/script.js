fa926275cdef987e200c1785bc7c33e5d9b5abcf
Sam <wk.sams@gmail.com>
2020-06-24 18:56:03 -0300

    improve performance

--- intermediate/meme-generator/scripts/script.js
+++ intermediate/meme-generator/scripts/script.js
@@ -1,36 +1,36 @@
 (function() {
     document.querySelector('#image-url').addEventListener('input', onChangeUrl);
     function onChangeUrl() {
-        imageUrl = this.value;
+        p.setImageUrl(this.value);
     }
 
     document.querySelector('#top-text').addEventListener('input', onChangeTopText);
     function onChangeTopText() {
-        topText = this.value;
+        p.setTopText(this.value);
     }
 
     document.querySelector('#top-text-color').addEventListener('input', onChangeTopTextColor);
     function onChangeTopTextColor() {
-        topTextColor = this.value;
+        p.setTopTextColor(this.value);
     }
 
     document.querySelector('#top-text-font-size').addEventListener('input', onChangeTopTextFontSize);
     function onChangeTopTextFontSize() {
-        topTextFontSize = parseInt(this.value);
+        p.setTopTextFontSize(parseInt(this.value));
     }
 
     document.querySelector('#bottom-text').addEventListener('input', onChangeBottomText);
     function onChangeBottomText() {
-        bottomText = this.value;
+        p.setBottomText(this.value);
     }
 
     document.querySelector('#bottom-text-color').addEventListener('input', onChangeBottomTextColor);
     function onChangeBottomTextColor() {
-        bottomTextColor = this.value;
+        p.setBottomTextColor(this.value);
     }
 
     document.querySelector('#bottom-text-font-size').addEventListener('input', onChangeBottomTextFontSize);
     function onChangeBottomTextFontSize() {
-        bottomTextFontSize = parseInt(this.value);
+        p.setBottomTextFontSize(parseInt(this.value));
     }
 })();
--- intermediate/meme-generator/scripts/sketch.js
+++ intermediate/meme-generator/scripts/sketch.js
@@ -1,43 +1,94 @@
-let imageUrl;
+let p = new p5((sketch) => {
+    let imageUrl;
 
-let topText = '';
-let topTextColor = '#000';
-let topTextFontSize = 16;
+    let topText = '';
+    let topTextColor = '#000';
+    let topTextFontSize = 16;
 
-let bottomText = '';
-let bottomTextColor = '#000';
-let bottomTextFontSize = 16;
+    let bottomText = '';
+    let bottomTextColor = '#000';
+    let bottomTextFontSize = 16;
 
-let width = 400;
-let height = 400;
+    let width = 400;
+    let height = 400;
 
-function setup() {
-    const canvas = createCanvas(width, height);
-    canvas.parent('image-editor');
-}
+    let needRedraw = false;
 
-function draw() {
-    if(imageUrl) {
-        loadImage(imageUrl, img => {
-            resizeCanvas(img.width, img.height);
-            image(img, 0, 0);
-            width = img.width;
-            height = img.height;
-        }, err => {
-            imageUrl = null;
-            resizeCanvas(width, height);
-        });
-    } else {
-        resizeCanvas(width, height);
+    sketch.setImageUrl = (value) => {
+        imageUrl = value;
+        needRedraw = true;
+        sketch.redraw();
     }
 
-    textAlign(CENTER, TOP);
-    textSize(topTextFontSize);
-    fill(topTextColor);
-    text(topText, 0, 0, width, height / 2);
+    sketch.setTopText = (value) => {
+        topText = value;
+        needRedraw = true;
+        sketch.redraw();
+    }
 
-    textAlign(CENTER, BOTTOM);
-    textSize(bottomTextFontSize);
-    fill(bottomTextColor);
-    text(bottomText, 0, height / 2, width, height / 2);
-}
+    sketch.setTopTextColor = (value) => {
+        topTextColor = value;
+        needRedraw = true;
+        sketch.redraw();
+    }
+
+    sketch.setTopTextFontSize = (value) => {
+        topTextFontSize = value;
+        needRedraw = true;
+        sketch.redraw();
+    }
+
+    sketch.setBottomText = (value) => {
+        bottomText = value;
+        needRedraw = true;
+        sketch.redraw();
+    }
+
+    sketch.setBottomTextColor = (value) => {
+        bottomTextColor = value;
+        needRedraw = true;
+        sketch.redraw();
+    }
+
+    sketch.setBottomTextFontSize = (value) => {
+        bottomTextFontSize = value;
+        needRedraw = true;
+        sketch.redraw();
+    }
+
+    sketch.setup = () => {
+        sketch.createCanvas(width, height);
+        sketch.noLoop();
+    }
+
+    sketch.draw = () => {
+        if(needRedraw) {
+            if(imageUrl) {
+                sketch.loadImage(imageUrl, img => {
+                    sketch.resizeCanvas(img.width, img.height);
+                    sketch.image(img, 0, 0);
+                    width = img.width;
+                    height = img.height;
+                    
+                    sketch.textAlign(sketch.CENTER, sketch.TOP);
+                    sketch.textSize(topTextFontSize);
+                    sketch.fill(topTextColor);
+                    sketch.text(topText, 0, 0, width, height / 2);
+
+                    sketch.textAlign(sketch.CENTER, sketch.BOTTOM);
+                    sketch.textSize(bottomTextFontSize);
+                    sketch.fill(bottomTextColor);
+                    sketch.text(bottomText, 0, height / 2, width, height / 2);   
+                }, err => {
+                    imageUrl = null;
+                    sketch.resizeCanvas(width, height);
+                });
+            } else {
+                sketch.resizeCanvas(width, height);
+            }
+
+            needRedraw = false;
+        }
+    }
+
+}, 'image-editor');
