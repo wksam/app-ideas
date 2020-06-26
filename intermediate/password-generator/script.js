ad9d1b6e0958e039e5d71b93afa85a36998f9f2b
Sam <wk.sams@gmail.com>
2020-06-26 13:34:06 -0300

    generate password

---
+++ intermediate/password-generator/script.js
@@ -0,0 +1,58 @@
+// (function() {
+    document.querySelector('form').addEventListener('submit', onSubmit);
+
+    function onSubmit(e) {
+        e.preventDefault();
+
+        const formData = new FormData(e.target);
+        const upper = formData.get('uppercase') === 'on';
+        const lower = formData.get('lowercase') === 'on';
+        const number = formData.get('numbers') === 'on';
+        const symbol = formData.get('symbols') === 'on';
+        const length = parseInt(formData.get('length'));
+
+        const password = generatePassword(upper, lower, number, symbol, length);
+
+        document.querySelector('#password').value = password;
+    }
+
+    const randomFunction = {
+        upper: generateUppercase,
+        lower: generateLowercase,
+        number: generateNumber,
+        symbol: generateSymbol
+    };
+
+    function generatePassword(upper, lower, number, symbol, length) {
+        let generatedPassword = '';
+        const types = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0]).map(item => Object.keys(item)[0]);
+        if(types.length === 0 || length === 0) return generatedPassword;
+
+        for (let i = 0; i < length; i++) {
+            generatedPassword += randomFunction[types[randomNumber(0, types.length - 1)]]();
+        }
+
+        return generatedPassword;
+    }
+
+    function generateUppercase() {
+        return String.fromCharCode(randomNumber(65, 132));
+    }
+
+    function generateLowercase() {
+        return String.fromCharCode(randomNumber(97, 122));
+    }
+
+    function generateNumber() {
+        return String.fromCharCode(randomNumber(48, 57));
+    }
+
+    function generateSymbol() {
+        const symbols = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~".split('');
+        return symbols[randomNumber(0, symbols.length - 1)];
+    }
+    
+    function randomNumber(min, max) {
+        return parseInt(Math.round(Math.random() * (max - min) + min));
+    }
+// })();
