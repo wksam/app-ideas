459b81d994ad69e2fb91ccca89668762554fc715
Sam <wk.sams@gmail.com>
2020-06-29 17:11:38 -0300

    create badge panel with qrcode and information

--- intermediate/qrcode-badge-generator/README.md
+++ intermediate/qrcode-badge-generator/README.md
@@ -38,14 +38,14 @@ and email address are required, but the Twitter and GitHub account names are
 optional fields.
 -   [x] User can click the 'Cancel' button to clear all input fields as well as
 the badge panel (see below) if it has been created.
--   [ ] User can click the 'Create' button to generated an image of the 
+-   [x] User can click the 'Create' button to generated an image of the 
 attendees name badge.
 -   [x] User can see an error message if any of the following are true:
     - Required fields are empty
     - A first name and last name have not been entered
     - Email input field isn't a properly formatted email address
     - Twitter account name doesn't start with '@'
--   [ ] User can see an badge panel displayed on screen containing this 
+-   [x] User can see an badge panel displayed on screen containing this 
 information, plus a QR code encoded with this information.
 
 ## Bonus features
---
+++ intermediate/qrcode-badge-generator/scripts/script.js
@@ -0,0 +1,43 @@
+(function() {
+    const typeNumberArray = [7, 14, 24, 34, 44, 58, 64, 84, 98, 119, 137, 155, 177, 194, 220, 250, 280, 310, 338, 382, 403, 439, 461, 511];
+
+    document.querySelector('form').addEventListener('submit', onSubmit);
+    function onSubmit(e) {
+        e.preventDefault();
+
+        const formData = new FormData(e.target);
+        const name = formData.get('name');
+        const email = formData.get('email');
+        const twitter = formData.get('twitter');
+        const github = formData.get('github');
+
+        const badgePanel = document.querySelector('#badge-panel');
+        badgePanel.hidden = false;
+        badgePanel.querySelector('#qrcode').innerHTML = createQRCode(name);
+        badgePanel.querySelector('#badge-name').textContent = name;
+        badgePanel.querySelector('#badge-email').textContent = email;
+        badgePanel.querySelector('#badge-twitter').textContent = twitter;
+        badgePanel.querySelector('#badge-github').textContent = github;
+    }
+
+    function createQRCode(name) {
+        const length = name.length;
+        const typeNumber = getErrorCorrectionLevel(length);
+        console.log(length, typeNumber);
+
+        const qr = qrcode(typeNumber, 'H');
+        qr.addData(name);
+        qr.make();
+
+        return qr.createImgTag();
+    }
+
+    function getErrorCorrectionLevel(length) {
+        return typeNumberArray.findIndex(item => item > length) + 1;
+    }
+
+    document.querySelector('form').addEventListener('reset', onReset);
+    function onReset() {
+        document.querySelector('#badge-panel').hidden = true;
+    }
+})();
