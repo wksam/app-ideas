f4adb432524c13ee30ab64fc379366240ec1bfbf
Sam <wk.sams@gmail.com>
2020-07-04 09:37:04 -0300

    Revert "add qrcode badge generator"
    This reverts commit 6ab986454a6a149f8e94045b380da10d17f07a4d.

--- intermediate/simple-online-store/scripts/products.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    document.querySelectorAll('.product').forEach(product => {
-        product.addEventListener('click', onSelectProduct);
-    });
-    
-    function onSelectProduct(e) {
-        console.log(e.currentTarget);
-    }
-})();
