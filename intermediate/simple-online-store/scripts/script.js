7ba66ce509f4ed5b6f777a6e38ca1825ef2885da
Sam <wk.sams@gmail.com>
2020-07-06 13:56:58 -0300

    add product list

---
+++ intermediate/simple-online-store/scripts/script.js
@@ -0,0 +1,48 @@
+(function() {
+    init();
+    function init() {
+        const productContainer = document.querySelector('.products');
+        productContainer.textContent = '';
+
+        for (const product of productList) {
+            productContainer.append(createProductCard(product));
+        }
+    }
+
+    function createProductCard(product) {
+        const container = document.createElement('div');
+        container.setAttribute('class', 'col mb-4');
+
+        const card = document.createElement('div');
+        card.setAttribute('class', 'card h-100');
+
+        const img = document.createElement('img');
+        img.setAttribute('class', 'card-img-top');
+        img.setAttribute('src', product.thumbnail);
+        img.setAttribute('alt', 'Product Image');
+
+        const cardBody = document.createElement('div');
+        cardBody.setAttribute('class', 'card-body');
+
+        const cardTitle = document.createElement('h5');
+        cardTitle.setAttribute('class', 'card-title text-primary');
+        cardTitle.textContent = product.name;
+
+        const cardDescription = document.createElement('p');
+        cardDescription.setAttribute('class', 'card-text text-black-50');
+        cardDescription.textContent = product.short_description;
+
+        const cardPrice = document.createElement('h4');
+        cardPrice.setAttribute('class', 'card-text text-secondary');
+        cardPrice.textContent = product.price;
+
+        container.append(card);
+        card.append(img);
+        card.append(cardBody);
+        cardBody.append(cardTitle);
+        cardBody.append(cardDescription);
+        cardBody.append(cardPrice);
+
+        return container;
+    }
+})();
