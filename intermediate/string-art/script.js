675c2ad569ab420dd40162c263fb8ffb346f395e
Sam <wk.sams@gmail.com>
2020-07-13 16:46:11 -0300

    add trail

--- intermediate/string-art/script.js
+++ intermediate/string-art/script.js
@@ -2,6 +2,7 @@
     let count = 0;
     let invert = false;
     let intervalId;
+    const lines = [];
 
     const canvas = document.querySelector('canvas');
     const context = canvas.getContext('2d');
@@ -12,17 +13,27 @@
         canvas.width = canvas.clientWidth;
         
         context.lineWidth = 3;
+        const line = {
+            moveTo: { x: 0, y: 0 },
+            lineTo: { x: 0, y: canvas.height },
+            color: { r: 0, g: 255, b: 255, a: 1 }
+        }
+        lines.push(line);
 
         clearInterval(intervalId);
         intervalId = setInterval(draw, 20);
     }
 
     function draw() {
-        context.clearRect(0, 0, canvas.width, canvas.height)
-        context.beginPath();
-        context.moveTo(count, 0);
-        context.lineTo(count, canvas.height);
-        context.stroke();
+        context.clearRect(0, 0, canvas.width, canvas.height);
+
+        for (const line of lines) {
+            context.beginPath();
+            context.moveTo(line.moveTo.x, line.moveTo.y);
+            context.lineTo(line.lineTo.x, line.lineTo.y);
+            context.strokeStyle = `rgba(${line.color.r}, ${line.color.g}, ${line.color.b}, ${line.color.a})`;
+            context.stroke();
+        }
 
         if(!invert) {
             if(count > canvas.width) {
@@ -35,8 +46,24 @@
         }
 
         if(!invert)
-            count++;
+            count += 5;
         else
-            count--;
+            count -= 5;
+
+        const line = {
+            moveTo: { x: count, y: 0 },
+            lineTo: { x: count, y: canvas.height },
+            color: { r: 0, g: 0, b: 0, a: 1 }
+        }
+        lines.push(line);
+
+        if(lines.length > 20)
+            lines.shift();
+
+        let alpha = 1;
+        for (let i = lines.length - 1; i >= 0; i--) {
+            lines[i].color.a = alpha;
+            alpha -= 0.05;
+        }
     }
 })();
