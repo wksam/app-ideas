af646a9666b0502c4a50f7aa6f423f8dfbf29879
Sam <wk.sams@gmail.com>
2020-07-16 14:02:37 -0300

    remove task

--- intermediate/to-do-app/script.js
+++ intermediate/to-do-app/script.js
@@ -19,7 +19,7 @@
 
     function createTask(task) {
         const listGroupItem = document.createElement('li');
-        listGroupItem.setAttribute('class', 'list-group-item');
+        listGroupItem.setAttribute('class', 'list-group-item d-flex justify-content-between align-items-center');
 
         const formCheck = document.createElement('div');
         formCheck.setAttribute('class', 'form-check');
@@ -36,9 +36,15 @@
         label.setAttribute('for', id);
         label.textContent = task;
 
+        const deleteButton = document.createElement('button');
+        deleteButton.setAttribute('class', 'btn btn-sm btn-outline-danger');
+        deleteButton.innerHTML = '<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z"/></svg>';
+        deleteButton.addEventListener('click', onRemove);
+
         listGroupItem.append(formCheck);
         formCheck.append(checkbox);
         formCheck.append(label);
+        listGroupItem.append(deleteButton);
 
         return listGroupItem;
     }
@@ -50,5 +56,9 @@
     function onCheck(e) {
         const formCheck = e.currentTarget.parentElement;
         formCheck.classList.toggle('checked');
+    }
+
+    function onRemove(e) {
+        e.currentTarget.parentElement.remove();
     }
 })();
