ba5fc40997cec0f595f36babb5eaa0f8c03ddea8
Sam <wk.sams@gmail.com>
2020-07-30 16:23:17 -0300

    add put ships

--- 3-advanced/battleship-game-engine/scripts/bge.js
+++ 3-advanced/battleship-game-engine/scripts/bge.js
@@ -10,16 +10,66 @@ class BGE {
 
     startGame() {
         this._board = this._generateBoard(this._size);
+        this._putShips(this._board, this._ships);
     }
 
     _generateBoard(size) {
-        return Array(size).fill(Array(size).fill(0));
+        return new Array(size).fill().map(() => Array(size).fill(0));
+    }
+
+    _getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min)) + min;
     }
 
     _getRandomPosition(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return [Math.floor(Math.random() * (max - min)) + min, Math.floor(Math.random() * (max - min)) + min];
+        return [this._getRandomInt(min, max), this._getRandomInt(min, max)];
+    }
+
+    _putShips(board, ships) {
+        let reposition = false;
+
+        for (const ship of ships) {
+            do {
+                const [positionY, positionX] = this._getRandomPosition(0, board.length);
+                const direction = this._getRandomInt(0, 2);
+
+                if(this._canBePlaced(board, ship, positionX, positionY, 0, direction)) {
+                    for (let i = 0; i < ship.size; i++) {
+                        switch (direction) {
+                            case 0:
+                                board[positionY][positionX + i] = ship.id;
+                                break;
+                            case 1:
+                                board[positionY + i][positionX] = ship.id;
+                                break;
+                        }
+                    }
+                    reposition = false;
+                } else {
+                    reposition = true;
+                }
+            } while (reposition);
+        }
+        console.log(board);
+    }
+
+    _canBePlaced(board, ship, x, y, currentSize, direction) {
+        if(currentSize < ship.size) {
+            if((x >= board.length || y >= board.length) || board[x][y] !== 0) return false;
+            currentSize++;
+            switch (direction) {
+                case 0:
+                    x++;
+                    return this._canBePlaced(board, ship, x, y, currentSize, direction);
+                case 1:
+                    y++;
+                    return this._canBePlaced(board, ship, x, y, currentSize, direction);
+            }
+        } else {
+            return true;
+        }
     }
 }
 
@@ -38,3 +88,4 @@ const ships = [{
 }];
 
 const gameEngine = new BGE(8, ships);
+gameEngine.startGame();
